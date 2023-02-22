9ab7941e89982b6859318cdda0c9488606db6218
oguzhanali <oguzxan.ali@gmail.com>
2023-02-22 16:50:32 +0300

    build: add workspace configuration

--- packages/client/pages/index.js
+++ packages/client/pages/index.js
@@ -1,10 +1,10 @@
-import Head from "next/head";
-import Layout, { siteTitle } from "../components/layout";
-import utilStyles from "../styles/utils.module.css";
-import { getSortedPostsData } from "../lib/posts";
-import Link from "next/link";
-import Date from "../components/date";
-import helper from "server";
+import Head from 'next/head'
+import Layout, { siteTitle } from '../components/layout'
+import utilStyles from '../styles/utils.module.css'
+import { getSortedPostsData } from '../lib/posts'
+import Link from 'next/link'
+import Date from '../components/date'
+import helper from 'server'
 
 export default function Home({ allPostsData }) {
   return (
@@ -30,14 +30,14 @@ export default function Home({ allPostsData }) {
         </ul>
       </section>
     </Layout>
-  );
+  )
 }
 
 export async function getStaticProps() {
-  const allPostsData = getSortedPostsData();
+  const allPostsData = getSortedPostsData()
   return {
     props: {
-      allPostsData,
-    },
-  };
+      allPostsData
+    }
+  }
 }
